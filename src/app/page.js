1f17add61e0c5e36ae30e79ca2f9eebaeb9cee7b
AndreMaia95 <andrefsm95@gmail.com>
2023-11-17 10:21:40 +0000

    fix: css to mobile

--- src/app/page.js
+++ src/app/page.js
@@ -17,18 +17,18 @@ export default function Home() {
   };
 
   return (
-    <main className="w-full h-screen flex flex-col items-center justify-center bg-gray-800">
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-[32px] font-bold pb-4 text-white">
+    <main className="w-full min-h-screen flex flex-col items-center justify-center bg-gray-800">
+      <div className="flex flex-col items-center justify-center w-full max-w-2xl p-4">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold pb-6 text-white">
           Random Joke Generator
         </h1>
-        <div className="w-[700px] bg-gray-500 p-4 rounded-md text-white flex flex-col items-center justify-between ">
+        <div className="w-full bg-gray-500 p-4 rounded-md text-white flex flex-col items-center justify-between">
           <div className="flex flex-col items-center mb-8">
-            <p className="text-[50px]">&#128540;</p>
+            <p className="text-3xl md:text-5xl mb-4">&#128540;</p>
             <div className="text-center">{joke}</div>
           </div>
           <button
-            className="border border-white py-2 px-4 rounded-md"
+            className="border border-white py-2 px-4 rounded-md hover:bg-white hover:text-gray-800 transition duration-200 ease-in-out"
             onClick={getJoke}
           >
             Generate Joke
